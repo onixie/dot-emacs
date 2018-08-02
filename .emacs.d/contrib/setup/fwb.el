5fbbab7715e7be3b543a209c94f4c12a486b109d
Yc.S <onixie@gmail.com>
2018-08-02 22:17:38 +0900

    Separate easy frame

--- .emacs.d/contrib/setup/fwb.el
+++ .emacs.d/contrib/setup/fwb.el
@@ -3,4 +3,44 @@
 (package-install 'maxframe)
 (require 'maxframe)
 
+;;;;;;;;;;;;;;;; Easy-Frame ;;;;;;;;;;;;;;;;
+(require 'easy-frame)
+(add-hook 'window-setup-hook 'easy-frame-mode)
+
+;; (when auto-smart-fill
+
+;;   ;; Speedbar
+;;   (defvar *speedbar-main-frame* nil)
+;;   (defvar *speedbar-main-frame-ffw-state* nil)
+;;   (defvar *speedbar-poped-up* nil)
+;;   (defvar *speedbar-takeup* 0.20)
+
+;;   (add-hook 'speedbar-after-create-hook
+;; 	    (lambda ()
+;; 	      (frame-fill-workarea nil 'right *speedbar-takeup*)
+;; 	      (define-key speedbar-mode-map (kbd "Q") 
+;; 		(lambda ()
+;; 		  (interactive)
+;; 		  (run-hooks 'speedbar-before-delete-hook)
+;; 		  (call-interactively 'delete-frame)))
+;; 	      (define-key speedbar-mode-map (kbd "q") (kbd "Q"))))
+
+;;   (add-hook 'speedbar-before-popup-hook
+;; 	    (lambda ()
+;; 	      (if (not *speedbar-poped-up*)
+;; 		  (progn
+;; 		    (setq auto-smart-fill nil)
+;; 		    (setq *speedbar-poped-up* t)
+;; 		    (setq *speedbar-main-frame* (selected-frame))
+;; 		    (setq *speedbar-main-frame-ffw-state* (frame-parameter *speedbar-main-frame* 'ffw-state))
+;; 		    (frame-fill-workarea *speedbar-main-frame* 'left (- 1 *speedbar-takeup*))))))
+
+;;   (add-hook 'speedbar-before-delete-hook 
+;; 	    (lambda ()
+;; 	      (setq *speedbar-poped-up* nil)
+;; 	      (setq auto-smart-fill t)
+;; 	      (frame-fill-workarea *speedbar-main-frame* *speedbar-main-frame-ffw-state*))))
+
+
+
 (provide 'setup/fwb)
