c6ee1a0315c58b3fb833cf898766c94890ae12bc
Yc.S <onixie@gmail.com>
2018-10-22 18:27:02 +0900

    Avoid old-style backquotes error for emacs27

--- .emacs.d/contrib/setup/goodies.el
+++ .emacs.d/contrib/setup/goodies.el
@@ -1,7 +1,10 @@
 (require 'setup/package)
 
-(package-install 'highlight-current-line)
-(require 'highlight-current-line)
+;; Workaround: since emacs27, old-style backquote syntax became an error
+;; Find other line highlighter or wait until someone solves the error.
+(when (< emacs-major-version 27)
+  (package-install 'highlight-current-line)
+  (require 'highlight-current-line))
 
 (package-install 'tabbar)
 (require 'tabbar)
