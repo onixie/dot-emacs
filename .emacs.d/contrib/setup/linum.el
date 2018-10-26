8ebe40d34119c1de16b2484da4bd87cd1462f15d
Yc.S <onixie@gmail.com>
2018-10-26 17:45:10 +0900

    No more need for linum text scaling workaround in emacs27

--- .emacs.d/contrib/setup/linum.el
+++ .emacs.d/contrib/setup/linum.el
@@ -1,30 +1,42 @@
 (require 'linum)
 
-(defun rescale-window-margin-by-faceremapping (win)
-  (let ((width (car (window-margins)))
-	(scale (cadr (assoc ':height 
-			    (assoc 'default 
-				   face-remapping-alist)))))
-    (if (numberp scale)
-	(progn
-	  (set-window-margins win (ceiling (* width scale)) (cdr (window-margins win)))
-	  (force-window-update))
-      nil)))
-
-(defadvice linum-update-window (after scaling-margin-width last (win) activate)
-  (rescale-window-margin-by-faceremapping win)
-  ad-return-value)
+(when (< emacs-major-version 27)
+  (progn
+    (defun rescale-window-margin-by-faceremapping (win)
+      (let ((width (car (window-margins)))
+	    (scale (cadr (assoc ':height 
+			        (assoc 'default 
+				       face-remapping-alist)))))
+        (if (numberp scale)
+	    (progn
+	      (set-window-margins win (ceiling (* width scale)) (cdr (window-margins win)))
+	      (force-window-update))
+          nil))
+      
+      )
+    (defadvice linum-update-window (after scaling-margin-width last (win) activate)
+      (rescale-window-margin-by-faceremapping win)
+      ad-return-value)))
 
 (mapc (lambda (mode-hook)
 	(add-hook mode-hook 'linum-on))
-      '(c-mode-hook c++-mode-hook sh-mode-hook lisp-mode-hook emacs-lisp-mode-hook lisp-interaction-mode-hook asm-mode-hook))
+      '(c-mode-hook
+        c++-mode-hook
+        sh-mode-hook
+        lisp-mode-hook
+        emacs-lisp-mode-hook
+        lisp-interaction-mode-hook
+        asm-mode-hook
+        haskell-mode))
 
 (with-current-buffer "*Messages*"
   (linum-on))
 
-(global-set-key (kbd "M-<mouse-4>") 'text-scale-increase)
+(global-set-key (kbd "M-+") 'text-scale-increase)
+(global-set-key (kbd "M-<mouse-4>") (kbd "M-+"))
 (global-set-key (kbd "<left-magin> <M-mouse-4>") (kbd "M-<mouse-4>"))
-(global-set-key (kbd "M-<mouse-5>") 'text-scale-decrease)
+(global-set-key (kbd "M--") 'text-scale-decrease)
+(global-set-key (kbd "M-<mouse-5>") (kbd "M--"))
 (global-set-key (kbd "<left-margin> <M-mouse-5>") (kbd "M-<mouse-5>"))
 
 (global-set-key (kbd "C-x M-v") 
