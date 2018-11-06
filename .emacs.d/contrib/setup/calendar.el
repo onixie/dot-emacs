19ae5a61b045343dec353b4f3318c11c9200cf74
Yc.S <onixie@gmail.com>
2018-11-06 18:49:04 +0900

    Preventing modify customizations from setup

--- .emacs.d/contrib/setup/calendar.el
+++ .emacs.d/contrib/setup/calendar.el
@@ -1,21 +1,15 @@
 
-(add-hook 'calendar-mode-hook
-          (lambda nil
-            (unless
-                (eq org-agenda-diary-file 'diary-file)
-              (define-key calendar-mode-map org-calendar-insert-diary-entry-key 'org-agenda-diary-entry))
-            (toggle-truncate-lines 1)
-            (load "~/.emacs.d/contrib/more-calendar.el")
-            (progn
-              (setq scroll-margin 0)
-              (setq scroll-step 0)
-              (setq scroll-conservatively 0))))
+(add-hook 'calendar-mode-hook (lambda nil
+                                (load "~/.emacs.d/contrib/more-calendar.el")
+                                (toggle-truncate-lines 1)
+                                (setq scroll-margin 0
+                                      scroll-step 0
+                                      scroll-conservatively 0)))
 
-(add-hook 'calendar-move-hook
-          (lambda nil
-            (progn
-              (calendar-update-mode-line)
-              (scroll-down))))
+(add-hook 'calendar-move-hook (lambda nil
+                                (progn
+                                  (calendar-update-mode-line)
+                                  (scroll-down))))
 
 (setq calendar-after-frame-setup-hook nil
       calendar-week-start-day 1
--- .emacs.d/contrib/setup/org.el
+++ .emacs.d/contrib/setup/org.el
@@ -2,16 +2,21 @@
 
 (require 'org)
 
-(custom-set-variables
- '(calendar-mode-hook (quote ((lambda nil (unless (eq org-agenda-diary-file (quote diary-file)) (define-key calendar-mode-map org-calendar-insert-diary-entry-key (quote org-agenda-diary-entry)))) (lambda nil (toggle-truncate-lines 1)) (lambda nil (load "~/.emacs.d/contrib/more-calendar.el")) (lambda nil (progn (setq scroll-margin 0) (setq scroll-step 0) (setq scroll-conservatively 0))))))
- '(org-export-docbook-xsl-fo-proc-command "fop %s %s")
- '(org-export-docbook-xslt-proc-command "xsltproc --output %s /usr/share/docbook2odf/xsl/docbook.xsl %s")
- '(org-hide-leading-stars nil nil nil "Clean View is done by org-startup-indented, this variable has little usage in future.")
- '(org-hierarchical-todo-statistics nil)
- '(org-startup-indented t))
+(add-hook 'calendar-mode-hook (lambda nil
+                                (unless (eq org-agenda-diary-file (quote diary-file))
+                                  (define-key calendar-mode-map org-calendar-insert-diary-entry-key (quote org-agenda-diary-entry)))))
 
-(custom-set-faces
- '(org-hide ((((background dark)) (:inherit default :foreground "default" :inverse-video t)))))
+;; After org v8, docbook is not included anymore. Use ox-textinfo instead
+(when (< (string-to-number (first (split-string org-version "\\."))) 8)
+  (require 'org-docbook)
+  (setq org-export-docbook-xsl-fo-proc-command "fop %s %s"
+	org-export-docbook-xslt-proc-command "xsltproc --output %s /usr/share/docbook2odf/xsl/docbook.xsl %s"))
+
+(setq org-hide-leading-stars nil
+      org-hierarchical-todo-statistics nil
+      org-startup-indented t)
+
+(face-spec-set 'org-hide '((((background dark)) (:inherit default :foreground "default" :inverse-video t))))
 
 (defun setup-org--org-summary-todo (n-done n-not-done)
   "Switch entry to DONE when all subentries are done, to TODO otherwise."
@@ -25,5 +30,6 @@
 
 (add-hook 'org-after-todo-statistics-hook 'setup-org--org-summary-todo)
 
+(org-babel-do-load-languages 'org-babel-load-languages '((shell . t)))
 
 (provide 'setup/org)
