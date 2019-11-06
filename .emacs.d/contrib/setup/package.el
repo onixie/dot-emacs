7792a3a187a04a1f1c77dba1b070d6d27a612521
Yc.S <onixie@gmail.com>
2019-11-06 18:10:27 +0900

    Improve startup time (refresh package contents once per day)

--- .emacs.d/contrib/setup/package.el
+++ .emacs.d/contrib/setup/package.el
@@ -7,6 +7,21 @@
 (when (< emacs-major-version 27)
   (package-initialize))
 
-(package-refresh-contents)
+(defcustom dotemacs-package-refresh-interval (* 60 60 24 1)
+  "Interval to trigger a package-refresh-contents call when starting emacs"
+  :type 'integer)
+
+(defun dotemacs--call-after (interval func)
+  (let ((last-refreshed-time nil))
+    (load "~/.emacs.d/last-refreshed-time.el" t)
+    (print last-refreshed-time)
+    (unless (and last-refreshed-time
+		 interval
+		 (time-less-p (time-subtract (current-time) last-refreshed-time)
+			      (seconds-to-time interval)))
+      (funcall func)
+      (write-region (format "(setq last-refreshed-time '%s)" (current-time)) nil "~/.emacs.d/last-refreshed-time.el") t)))
+
+(dotemacs--call-after dotemacs-package-refresh-interval #'package-refresh-contents)
 
 (provide 'setup/package)
