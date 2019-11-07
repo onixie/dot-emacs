827962ec5172d841e67b13964ea1e459bafc50ac
Yc.S <onixie@gmail.com>
2019-11-07 11:18:39 +0900

    Fix hanging tramp when opening remote shell

--- .emacs.d/contrib/setup/shell.el
+++ .emacs.d/contrib/setup/shell.el
@@ -1,18 +1,18 @@
-
-(cl-defun dotemacs--open-shell (path &optional (always-new t))
-  (cl-labels ((get-buffer-name (path)
-                               (let ((name system-name)
-                                     (pos 0))
-                                 (block nil
-                                   (while (or (string-match ":\\([^:|/]+\\)\\(?:[:|/]\\|$\\)" path pos)
-                                              (return name))
-                                     (setq name (match-string 1 path)
-                                           pos (match-end 0))))))) 
-    (let ((default-directory path)
-          (buffer-name (get-buffer-name path)))
-      (shell (if always-new
-                 (generate-new-buffer-name buffer-name)
-               buffer-name)))))
+(defun dotemacs--open-shell (path)
+  (cl-labels ((calc-buffer-name (path)
+               (let ((name system-name)
+                     (pos 0))
+                 (block nil
+                   (while (or (string-match ":\\([^:|/]+\\)\\(?:[:]\\)" path pos)
+                              (return name))
+                     (setq name (match-string 1 path)
+                           pos (match-end 0)))))))
+    (let ((buffer (generate-new-buffer (calc-buffer-name path))))
+      (setf (buffer-local-value 'default-directory buffer) 
+            (if (string-suffix-p ":" path) 
+                (concat path "/") ;Workaround for tramp hang issue
+              path))
+      (shell buffer))))
 
 (add-hook 'shell-mode-hook (dot-emacs--kill-buffer-and-window-when-process "\\(?:finished\\|exited\\|killed\\|quit\\)"))
 
