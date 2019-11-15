4e0d20e1cdfd00ee0da5f602db9dc9507e0ecaee
Yc.S <onixie@gmail.com>
2019-11-15 11:23:16 +0900

    Enable minions to make minor modes managable on mode lines

--- .emacs.d/contrib/setup/theme.el
+++ .emacs.d/contrib/setup/theme.el
@@ -63,12 +63,72 @@
 (face-spec-set 'ac-yasnippet-candidate-face '((t (:background "sandybrown" :foreground "black" :height 120))))
 (face-spec-set 'ac-yasnippet-selection-face '((t (:background "coral3" :foreground "white" :height 120))))
 
+;;;;;;;;;;;;;;;; Minions Line ;;;;;;;;;;;;;;;;
+(package-install 'minions)
+(require 'minions)
+
 ;;;;;;;;;;;;;;;; Smart Mode Line ;;;;;;;;;;;;;;;;
 (package-install 'smart-mode-line)
 (package-install 'smart-mode-line-powerline-theme)
 (setq sml/no-confirm-load-theme t)
 (sml/setup)
-(powerline-default-theme)
+
+(defun dotemacs--powerline-center-minions-theme ()
+  "Setup a mode-line with major and minor modes centered."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face0 (if active 'powerline-active0 'powerline-inactive0))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw "%*" face0 'l)
+                                     (when powerline-display-buffer-size
+                                       (powerline-buffer-size face0 'l))
+                                     (powerline-buffer-id `(mode-line-buffer-id ,face0) 'l)
+                                     (powerline-raw " " face0)
+                                     (funcall separator-left face0 face1)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-vc face1)))
+                          (rhs (list (when (window-full-width-p)
+                                       (powerline-raw global-mode-string face1 'r))
+                                     (powerline-raw "%4l" face1 'r)
+                                     (powerline-raw ":" face1)
+                                     (powerline-raw "%3c" face1 'r)
+                                     (funcall separator-right face1 face0)
+                                     (powerline-raw " " face0)
+                                     (powerline-raw "%6p" face0 'r)
+                                     (when powerline-display-hud
+                                       (powerline-hud face2 face1))
+                                     (powerline-fill face0 0)))
+                          (center (list (powerline-raw " " face1)
+                                        (funcall separator-left face1 face2)
+                                        (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                          (powerline-raw erc-modified-channels-object face2 'l))
+                                        (if (bound-and-true-p minions-mode)
+                                            (powerline-raw minions-mode-line-modes face2)
+                                          (powerline-major-mode face2 'l)
+                                          (powerline-process face2)
+                                          (powerline-raw " :" face2)
+                                          (powerline-minor-modes face2 'l)
+                                          (powerline-raw " " face2))
+                                        (funcall separator-right face2 face1))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill-center face1 (/ (powerline-width center) 2.0))
+                             (powerline-render center)
+                             (powerline-fill face1 (powerline-width rhs))
+                             (powerline-render rhs)))))))
+
+(dotemacs--powerline-center-minions-theme)
 
 ;;;;;;;;;;;;;;;; Highlight Current Lines ;;;;;;;;;;;;;;;;
 ;; Workaround: since emacs27, old-style backquote syntax became an error
