52fb8bacd3de3a92769707a3c00665e492560507
Yc.S <onixie@gmail.com>
2019-12-01 13:45:23 +0900

    Fix syntax error

--- setup/app/irc.el
+++ setup/app/irc.el
@@ -12,6 +12,6 @@
   (erc-interpret-mirc-color t)
   (erc-kill-buffer-on-part t)
   (erc-kill-queries-on-quit t)
-  (erc-kill-server-buffer-on-quit t)))
+  (erc-kill-server-buffer-on-quit t))
 
 (provide 'setup/app/irc)
