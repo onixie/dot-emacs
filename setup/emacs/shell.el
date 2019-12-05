16169332cfb2a54f502b9b74b1a25f12f6ef1ca1
Yc.S <onixie@gmail.com>
2019-12-05 12:11:07 +0900

    Add color to shell mode

--- setup/emacs/shell.el
+++ setup/emacs/shell.el
@@ -1,5 +1,9 @@
 
 (use-package setup/base/defs
-  :hook ((shell-mode term-mode) . dot-emacs::kill-buffer-and-window-on-process-die))
+  :hook
+  (((shell-mode term-mode) . dot-emacs::kill-buffer-and-window-on-process-die)
+   ((shell-mode term-mode) . ansi-color-for-comint-mode-on)
+   (shell-mode . (lambda ()
+                   (face-remap-set-base 'comint-highlight-prompt :inherit nil)))))
 
 (provide 'setup/emacs/shell)
