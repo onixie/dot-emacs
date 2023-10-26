3deda3e7f5640dc1a9483d2571089ac605039849
Yc.Shen <onixie@gmail.com>
2023-10-26 17:30:17 +0900

    support toggle/untoggle xml tags

--- setup/lang/markup.el
+++ setup/lang/markup.el
@@ -3,4 +3,18 @@
 
 (use-package systemd :ensure t)
 
+(use-package nxml-mode
+  :config
+  (require 'hideshow)
+  (add-to-list 'hs-special-modes-alist
+               '(nxml-mode
+                 "<!--\\|<[^/>]*[^/]>"
+                 "-->\\|</[^/>]*[^/]>"))
+  :bind
+  (:map nxml-mode-map
+        ("<backtab>" . hs-hide-block)
+        ("<C-tab>" . hs-show-block))
+  :hook
+  ((nxml-mode . hs-minor-mode)))
+
 (provide 'setup/lang/markup)
